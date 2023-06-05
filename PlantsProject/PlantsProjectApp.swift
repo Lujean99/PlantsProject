524511f71765c1624f8178df3c816889de516353
Lujean99 <116796683+Lujean99@users.noreply.github.com>
2023-06-05 19:59:59 +0300

    Initial Commit

---
+++ PlantsProject/PlantsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsProjectApp.swift
+//  PlantsProject
+//
+//  Created by Lujeen Alradhi on 16/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
